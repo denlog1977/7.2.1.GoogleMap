4da4d320e0772b9cb19f567742b35e85d1287c41
denlog1977 <denlog1977@gmail.com>
2019-12-09 16:54:45 +0300

    MY_PERMISSIONS_REQUEST_SEND_SMS

--- app/src/main/java/com/example/a721googlemap/MainActivity.java
+++ app/src/main/java/com/example/a721googlemap/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.a721googlemap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 11;
+        private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 12;
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -62,17 +68,51 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                EditText editText = findViewById(R.id.editTextPhoneNumber);
-                String location = "";
-                String editTextString = editText.getText().toString();
-
-                if (editTextString.length() == 0) {
-                    Toast.makeText(MainActivity.this, R.string.TypePhoneNumber, Toast.LENGTH_SHORT).show();
-                    return;
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // Разрешение не получено. Делаем запрос на добавление разрешения звонка
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                } else { // Разрешение уже получено
+                    EditText editText = findViewById(R.id.editTextPhoneNumber);
+    //                String editTextString = editText.getText().toString();
+                    String editTextString = "tel:+7 (905) 370-30-00";
+                    if (editTextString.length() == 0) {
+                        Toast.makeText(MainActivity.this, R.string.TypePhoneNumber, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(editTextString));
+                    startActivity(intent);
                 }
 
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(editTextString));
-                startActivity(intent);
+            }
+        });
+
+        Button buttonSMS = findViewById(R.id.buttonSMS);
+        buttonSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    // Разрешение не получено. Делаем запрос на добавление разрешения звонка
+                    ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SEND_SMS);
+                } else {
+
+                    // Разрешение уже получено
+
+                    EditText editText = findViewById(R.id.editTextMessage);
+                    //String editTextString = editText.getText().toString();
+                    String editTextString = "Привет от Деника! Далее Тект Отправленного СМС.";
+
+                    if (editTextString.length() == 0) {
+                        Toast.makeText(MainActivity.this, R.string.inputTextMessage, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage("+79053703000",null,editTextString,null,null);
+
+    //                Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(editTextString));
+    //                startActivity(intent);
+                }
             }
         });
 
@@ -81,3 +121,4 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
+
